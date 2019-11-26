03942b02821180c9e9e9d01e840503ff8c57a27a
Maxim Puchkov <mpuchkov@sfu.ca>
2019-11-25 16:55:35 -0800

    Adding preset network diagrams to docs/

---
+++ docs/Presets/Network 1 - Baby /baby.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+
+from mininet.net import Mininet
+from mininet.node import Controller, RemoteController, OVSController
+from mininet.node import CPULimitedHost, Host, Node
+from mininet.node import OVSKernelSwitch, UserSwitch
+from mininet.node import IVSSwitch
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+from mininet.link import TCLink, Intf
+from subprocess import call
+
+def myNetwork():
+
+    net = Mininet( topo=None,
+                   build=False,
+                   ipBase='10.0.0.0/8')
+
+    info( '*** Adding controller\n' )
+    c0=net.addController(name='c0',
+                      controller=Controller,
+                      protocol='tcp',
+                      port=6653)
+
+    info( '*** Add switches\n')
+    s3 = net.addSwitch('s3', cls=OVSKernelSwitch)
+    s2 = net.addSwitch('s2', cls=OVSKernelSwitch)
+    s1 = net.addSwitch('s1', cls=OVSKernelSwitch)
+
+    info( '*** Add hosts\n')
+    h1 = net.addHost('h1', cls=Host, ip='10.0.0.1', defaultRoute=None)
+    h3 = net.addHost('h3', cls=Host, ip='10.0.0.3', defaultRoute=None)
+    h2 = net.addHost('h2', cls=Host, ip='10.0.0.2', defaultRoute=None)
+
+    info( '*** Add links\n')
+    net.addLink(s2, s3)
+    net.addLink(s1, s3)
+    net.addLink(s1, s2)
+    net.addLink(s1, h1)
+    net.addLink(s2, h2)
+    net.addLink(s3, h3)
+
+    info( '*** Starting network\n')
+    net.build()
+    info( '*** Starting controllers\n')
+    for controller in net.controllers:
+        controller.start()
+
+    info( '*** Starting switches\n')
+    net.get('s3').start([c0])
+    net.get('s2').start([c0])
+    net.get('s1').start([c0])
+
+    info( '*** Post configure switches and hosts\n')
+
+    CLI(net)
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel( 'info' )
+    myNetwork()
+
